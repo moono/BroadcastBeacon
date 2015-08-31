3c992538d4cd1db8e1ded040b9cd33e345614e51
mookyung song <mookyung@HCs-MacBook-Pro.local>
2015-08-31 14:13:11 +0900

    Initial Commit

---
+++ BroadcastBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BroadcastBeacon
+//
+//  Created by mookyung song on 8/31/15.
+//  Copyright (c) 2015 moono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BroadcastBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BroadcastBeacon
+//
+//  Created by mookyung song on 8/31/15.
+//  Copyright (c) 2015 moono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
